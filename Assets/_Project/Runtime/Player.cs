aaab0916c21efa88f15c033ee42e0e9d05ed9aa7
Elie GAZEL <elie@gazel.net>
2025-04-29 09:59:09 +0200

    initial commit

---
+++ Assets/_Project/Runtime/Player.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerCharacter playerCharacter;
+    [SerializeField] private PlayerCamera playerCamera;
+    [Space]
+    [SerializeField] private CameraSpring cameraSpring;
+    [SerializeField] private CameraLean cameraLean;
+    [SerializeField] private Volume volume;
+    [SerializeField] private StanceVignette stanceVignette;
+
+    private PlayerInputActions _inputActions;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        _inputActions = new PlayerInputActions();
+        _inputActions.Enable();
+
+        playerCharacter.Initialize();
+        playerCamera.Initialize(playerCharacter.GetCameraTarget());
+
+        cameraSpring.Initialize();
+        cameraLean.Initialize();
+
+        stanceVignette.Initialize(volume.profile);
+    }
+
+    private void OnDestroy()
+    {
+        _inputActions.Dispose();
+    }
+
+    void Update()
+    {
+        var input = _inputActions.Gameplay;
+        var deltaTime = Time.deltaTime;
+        // Get camera input and update its rotation
+        var cameraInput = new CameraInput { Look = input.Look.ReadValue<Vector2>() };
+        playerCamera.UpdateRotation(cameraInput);
+
+        // Get character input and update it
+        var characterInput = new CharacterInput
+        {
+            Rotation = playerCamera.transform.rotation,
+            Move = input.Move.ReadValue<Vector2>(),
+            Jump = input.Jump.WasPressedThisFrame(),
+            JumpSustain = input.Jump.IsPressed(),
+            Crouch = input.Crouch.WasPressedThisFrame()
+                ? CrouchInput.Toggle
+                : CrouchInput.None
+        };
+        playerCharacter.UpdateInput(characterInput);
+        playerCharacter.UpdateBody(deltaTime);
+
+#if UNITY_EDITOR
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            var ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+            if (Physics.Raycast(ray, out var hit))
+            {
+                Teleport(hit.point);
+            }
+        }
+#endif
+    }
+
+    private void LateUpdate()
+    {
+        var deltaTime = Time.deltaTime;
+        var cameraTarget = playerCharacter.GetCameraTarget();
+        var state = playerCharacter.GetState();
+        playerCamera.UpdatePosition(cameraTarget);
+        cameraSpring.UpdateSpring(deltaTime, cameraTarget.up);
+        cameraLean.UpdateLean
+            (
+                deltaTime,
+                state.Stance is Stance.Slide,
+                state.Acceleration,
+                cameraTarget.up
+            );
+        stanceVignette.UpdateVignette(deltaTime, state.Stance);
+    }
+
+    public void Teleport(Vector3 position)
+    {
+        playerCharacter.SetPosition(position);
+    }
+}
